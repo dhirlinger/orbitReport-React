75c3cfbf928d377486779ed6a31020363ef6b5ee
Doug Hirlinger <doughirlinger@gmail.com>
2024-08-21 10:56:28 -0400

    through task 5 test passing

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,34 @@
 const Table = ({ sat }) => {
+  const tableDisplay = sat.map(data => 
+    <tr key={data.id}>
+      <td>
+        {data.name}
+      </td>
+      <td>
+        {data.type}
+      </td>
+      <td>
+        {data.launchDate}
+      </td>
+      <td>
+       {`${data.operational ? "Active" : "Inactive"}`}
+      </td>
+    </tr>
+  );
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableDisplay}
+      </tbody>
+    </table>
   );
 };
 
